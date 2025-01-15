9f3b30e34a52af36f49f8d4a2585274eb0157a80
Yehan <xue200212@gmail.com>
2025-01-15 10:42:53 +0100

    base

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Web
-Hola
+Hola 
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import uvicorn
 
+
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+
 app = FastAPI()
+
+app.mount("/static",StaticFiles(directory="static"), name = "static" ) 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/test", response_class=HTMLResponse)
+async def test(request:Request):
+    
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"nombre":"yehan"}
+
+    )
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
