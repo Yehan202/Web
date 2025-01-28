672181f7ff6ddb5f5c10dca041a62ef10219b255
Yehan <xue200212@gmail.com>
2025-01-28 10:50:17 +0100

    1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI,Request
+import uvicorn
+
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static",StaticFiles(directory="static"), name = "static" ) 
+app.mount("/static",StaticFiles(directory="static"), name = "static") 
 templates = Jinja2Templates(directory="templates")
 
 # @app.get("/")
